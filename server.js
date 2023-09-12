9dfd962ee85864bc15c83acc3c43fd5edce6d8e5
LestterX <lgs.alves@hotmail.com>
2023-09-12 19:16:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+app.use(express.static('./public'));
+
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    return res.send()
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected --> ' + socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        msg = `${socket.id} ENVIOU: ${msg}`
+        console.log(msg);
+        io.emit('chat message', msg);
+    })
+});
+
+server.listen(5050, () => {
+    console.log('server running at http://localhost:3000');
+});
